b205037e9d3ff095c4df0137ca382223187c1be9
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2022-05-29 18:58:12 -0700

    priority queue demo works

--- projects/edf_scheduling/src/main.cpp
+++ projects/edf_scheduling/src/main.cpp
@@ -12,8 +12,10 @@ public:
     /**
      * \brief constructor
      */
-    Task(uint32_t update_interval_us)
-    :update_interval_us_{update_interval_us}
+    Task(uint32_t update_interval_us, const char* task_name)
+    :update_interval_us_{update_interval_us},
+     task_name_{task_name},
+     next_update_time_{99}
     {};
 
     /**
@@ -35,11 +37,19 @@ public:
         // ...
 
         // Record next time to update.
+        //printf("before update. next update time is %d\r\n",next_update_time_);
         next_update_time_ += update_interval_us_;
+        printf("%s updated. Next update @ time: %d\r\n", task_name_, next_update_time_);
     }
 
 
     uint32_t next_update_time_;
+    const char* task_name_;
+
+    /**
+     * \brief disable copy constructor.
+     */
+    //Task(const Task&) = delete;
 
 private:
     uint32_t update_interval_us_;
@@ -48,14 +58,17 @@ private:
 
 // Task object container
 Task tasks[TASK_COUNT]
-    {Task(1e6),     // 1 second update interval.
-     Task(1e6),     // 1 second update interval.
-     Task(2e6)};    // 2 second update interval.
+    {{1000000, "task0"},     // 1 second update interval.
+     {2000000, "task1"},     // 1 second update interval.
+     {3000000, "task2"}};    // 2 second update interval.
 
 
 // Priority queue of Task references.
 // Sorting relies on the item type implementing the '<' operator
-etl::priority_queue<Task, TASK_COUNT> pqueue{};
+etl::priority_queue<std::reference_wrapper<Task>,
+                    TASK_COUNT,
+                    etl::vector<std::reference_wrapper<Task>, TASK_COUNT>,
+                    etl::greater<std::reference_wrapper<Task>>> pqueue{};
 
 int main()
 {
@@ -65,23 +78,25 @@ int main()
     while (!stdio_usb_connected()){}
 
 // Load the priority queue.
-    for (auto task : tasks)
+    for (auto& task : tasks)
         pqueue.push(task);
 
 // Kick off the schedule.
     const uint32_t& curr_time_us = uint32_t(get_absolute_time());
-    for (auto task : tasks)
+    printf("starting tasks at: %d\r\n", curr_time_us);
+    for (auto& task : tasks)
         task.start_task(curr_time_us);
 
 // Run the schedule.
     while (true)
     {
         const uint32_t& curr_time_us = uint32_t(get_absolute_time());
-        Task& curr_task = pqueue.top();
+        auto& curr_task = pqueue.top().get();
         if (curr_time_us < curr_task.next_update_time_)
             continue;
-        curr_task.update();
         pqueue.pop();
+        printf("update @ %d. | ", curr_time_us);
+        curr_task.update();
         pqueue.push(curr_task);
     }
 }
