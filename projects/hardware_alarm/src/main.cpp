ea92868369e28c265da0f072b414680b69675712
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2024-01-26 17:58:49 -0800

    refactor hardware alarm to use mostly register writes

--- projects/hardware_alarm/src/main.cpp
+++ projects/hardware_alarm/src/main.cpp
@@ -1,10 +1,23 @@
 #include <pico/stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <hardware/timer.h>
 
-void alarm_callback_fn(uint alarm_num)
+uint32_t alarm_num = 0;
+
+
+inline uint32_t hardware_alarm_irq_number(uint32_t alarm_num)
+{
+    return TIMER_IRQ_0 + alarm_num;
+}
+
+void alarm_callback_fn(void)
 {
     gpio_put(25, 1); // Turn on LED from callback.
+    // Clear the latched interrupt.
+    irq_clear(hardware_alarm_irq_number(alarm_num));
+    uint irq_num = hardware_alarm_irq_number(alarm_num);
+    irq_remove_handler(irq_num, alarm_callback_fn);
 }
 
 int main()
@@ -17,11 +30,16 @@ int main()
     gpio_set_dir(25, true); // output
     gpio_put(25, 0);
 
-    int alarm_num = hardware_alarm_claim_unused(true); // required = true;
-    hardware_alarm_set_target(alarm_num, time_us_64() + 1000000); // Set 1 sec from now.
-    hardware_alarm_set_callback(alarm_num, alarm_callback_fn); // returns true if we missed the target.
+    //int alarm_num = hardware_alarm_claim_unused(true); // required = true;
+    //hardware_alarm_set_target(alarm_num, time_us_64() + 1000000); // Set 1 sec from now.
+    //hardware_alarm_set_callback(alarm_num, alarm_callback_fn); // returns true if we missed the target.
 
-    // TODO: do we need to: hardware_alarm_unclaim ??
+    // Note: does not handle edge case if we schedule alarm to happen *now*
+    uint32_t irq_num = hardware_alarm_irq_number(alarm_num);
+    irq_set_exclusive_handler(irq_num, alarm_callback_fn); // attach interrupt to function.
+    irq_set_enabled(irq_num, true); // enable alarm0 interrupt
+    timer_hw->inte |= (1u << alarm_num); // enable specified alarm to trigger interrupt.
+    timer_hw->alarm[alarm_num] = (time_us_32() + uint32_t(1000000)); // write time (also arms the alarm)
 
     while(true){}
 }
